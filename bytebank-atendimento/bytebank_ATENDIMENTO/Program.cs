10283373872e521e5690f86edd20e9038080fed2
João Sortudo <luckyjoey13@gmail.com>
2024-02-10 05:26:21 -0300

    Arquivos do curso

---
+++ bytebank-atendimento/bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+//TestaArrayInt();
+//void TestaArrayInt()
+//{
+//    int[] idades = { 30, 40, 17, 21, 18 };
+//    Console.WriteLine($"Tamanho do Array: {idades.Length}");
+
+//    for (int i = 0; i < idades.Length; i++)
+//    {
+//        Console.WriteLine($"Indice [{i}] = {idades[i]}");
+//    }
+
+//    Console.WriteLine($"Média de idades: {idades.Average()}");
+//}
+
+
+void MediaDaAmostra(double[] amostra)
+{
+    double media = 0;
+    double acumulador = 0;
+
+    if ((amostra == null) || (amostra.Length == 0))
+    {
+        Console.WriteLine("Amostra de dados nula ou vazia.");
+    }
+    else
+    {
+        for (int i = 0; i < amostra.Length; i++)
+        {
+            acumulador = acumulador + amostra[i];
+        }
+        media = acumulador / amostra.Length;
+        Console.WriteLine($"a média é {media}");
+    }
+
+
+}
+
+double[] valores = { 2.4, 10, 5.7, 15 };
+MediaDaAmostra(valores);
+
+//double CalculaMedia(double[] valores)
+//{
+//    double media = 0;
+//    double acumulador = 0;
+
+//    if ((valores == null) || (valores.Length == 0))
+//    {
+//        Console.WriteLine("Amostra de dados nula ou vazia.");
+//        return 0;
+//    }
+//    else
+//    {
+//        for (int i = 0; i < valores.Length; i++)
+//        {
+//            acumulador = acumulador + valores[i];
+//        }
+//        media = acumulador / valores.Length;
+//    }
+
+//    return media;
+//}
+
